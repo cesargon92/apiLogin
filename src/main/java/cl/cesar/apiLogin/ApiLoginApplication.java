7e37036ee48fbb9d16c12d952f368885129772ab
César González <cgonzalez@sentra.cl>
2019-11-26 17:45:04 -0300

    segundo commit agregando fuentes iniciales proyecto

---
+++ src/main/java/cl/cesar/apiLogin/ApiLoginApplication.java
@@ -0,0 +1,13 @@
+package cl.cesar.apiLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/cesar/apiLogin/ApiLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.cesar.apiLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
